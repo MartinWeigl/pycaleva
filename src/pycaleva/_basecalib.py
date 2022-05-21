Remove scaling of brier score and minor bug fixes

src/pycaleva/_basecalib.py
@@ -129,7 +129,7 @@ class _BaseCalibrationEvaluator:
         
         # Define calibration metrics
         self.__auroc = auroc(self.__y, self.__p)            # Area under the receiver operating curve
-        self.__brier = brier(self.__y, self.__p, True)      # Brier score scaled to [0.0 - 1.0]
+        self.__brier = brier(self.__y, self.__p,)           # Brier score
         self.__ace = None                                   # Adative calibration error
         self.__mce = None                                   # Maximum calibration error
         self.__awlc = None                                  # Area within lowess curve
@@ -306,7 +306,7 @@ class _BaseCalibrationEvaluator:
 
         if update_awlc:
             diff = np.abs(x_nonparametric - y_nonparametric)
-            self.__awlc = integrate.trapezoid(diff, y_nonparametric) # Area within loss curve
+            self.__awlc = integrate.trapezoid(diff, x_nonparametric) # Area within loss curve
             
         return (x_nonparametric, y_nonparametric)
 
@@ -402,12 +402,29 @@ class _BaseCalibrationEvaluator:
         
         # Sort Values according to their probability
         df = df.sort_values('prob')
+        df.reset_index(inplace=True)
         
         # Group data using deciles of risks
         try:
             df['dcl'] = pd.qcut(df['prob'], self.__ngroups)
         except ValueError:
-            raise Exception("Could not create groups. Maybe try with a lower number of groups or set n_groups to 'auto'.")
+            # Most likely low variance in probabilities results in non unique bin edges
+            try:
+                # FIX -> Put some probabilities into the same group
+                df['dcl'] = pd.qcut(df['prob'].rank(method='first'), self.__ngroups)
+
+                # Get propper interval labels after applied fix
+                new_categories = {}
+                df['rank'] = df['prob'].rank(method='first')
+                #for bin in set(df.dcl):
+                    #if bin.right > len(df.prob)-1:
+                        #new_categories[bin] = pd.Interval(left=round(df.prob[int(bin.left)],3), right=round(df.prob.iloc[-1],3))
+                    #else:
+                        #new_categories[bin] = pd.Interval(left=round(df.prob[int(bin.left)],3), right=round(df.prob[int(bin.right)],3))
+                    
+                #df['dcl'] = df['dcl'].cat.rename_categories(new_categories)
+            except ValueError:
+                raise Exception("Could not create groups. Maybe try with a lower number of groups or set n_groups to 'auto'.")
         except BaseException as err:
             print(f"Unexpected {err=}, {type(err)=}")
             raise
@@ -836,6 +853,7 @@ class _BaseCalibrationEvaluator:
 
         """
         fig, ax1 = plt.subplots(figsize=(10,6))
+        ax1.set_ylim(0.0, 1.0)
 
         # Draw a calibration plot using matplotlib only
         y_grouped = self.__ct["mean_observed"]
@@ -845,7 +863,7 @@ class _BaseCalibrationEvaluator:
         x_nonparametric,y_nonparametric = self.__nonparametric_fit(update_awlc=False)
 
         # Add calibration line for model
-        plt.scatter(p_grouped,y_grouped, marker="^", facecolors='none', edgecolors='r', label='Grouped observations')
+        plt.scatter(p_grouped,y_grouped, marker="^", facecolors='none', edgecolors='r', label=f'Grouped observations g={self.__ngroups}')
 
         # Add histogram on second axis
         h, e = np.histogram(self.__p, bins=50)

src/pycaleva/calbelt.py
@@ -67,8 +67,8 @@ def cdf_m(T:float, m:int, outsample:bool, alpha:float):
                 integrand1 = ( lambda y: (chi2.cdf(T - y, df = 1) - 1 + pDegInc) * chi2.pdf(y, df = 1) )
                 integrand2 = ( lambda y: (sqrt(T - y) - sqrt(k)) * 1/sqrt(y) )
 
-                integral1 = integrate(integrand1, lower = k, upper = T - k)[0]
-                integral2 = integrate(integrand2, lower = k, upper = T - k)[0]
+                integral1 = integrate(integrand1, a = k, b = T - k)[0]
+                integral2 = integrate(integrand2, a = k, b = T - k)[0]
 
                 num = (integral1 - exp(-T/2)/(2 * pi) * 2 * integral2)
                 den = pDegInc**2
@@ -85,7 +85,7 @@ def cdf_m(T:float, m:int, outsample:bool, alpha:float):
                     2)))) )
                 )
                 
-                integral = integrate(integrand, lower = sqrt(3 * k), upper = sqrt(T))[0]
+                integral = integrate(integrand, a = sqrt(3 * k), b = sqrt(T))[0]
 
                 return ((2/(pi * pDegInc**2))**(3/2) * integral)
 
@@ -110,7 +110,7 @@ def cdf_m(T:float, m:int, outsample:bool, alpha:float):
                 # INTERNAL m=3
                 integrand = ( lambda r: (r * exp(-(r**2)/2) * acos(sqrt(k)/r)) )
                 
-                integral = integrate(integrand, lower = sqrt(k), upper = sqrt(T))[0]
+                integral = integrate(integrand, a = sqrt(k), b = sqrt(T))[0]
                 return (2/(pi * pDegInc) * integral)
 
             elif m == 4:
@@ -120,7 +120,7 @@ def cdf_m(T:float, m:int, outsample:bool, alpha:float):
                                 2)) - sqrt(k)/r * acos((r**2/k - 1)**(-1/2))))
                             )
                 
-                integral = integrate(integrand, lower = sqrt(2 * k), upper = sqrt(T))[0]
+                integral = integrate(integrand, a = sqrt(2 * k), b = sqrt(T))[0]
                 return ((2/pi)**(3/2) * (pDegInc)**(-2) * integral)
             else:
                 # INTERNAL m>4 is not defined!
@@ -215,6 +215,7 @@ class CalibrationBelt():
             self.__y = y_true
             self.__p = y_pred
 
+        self.__clip_probs()
         self.__n = len(self.__y)
 
         # Warn user at internal evaluation
@@ -252,7 +253,11 @@ class CalibrationBelt():
 
         return True
 
+    # Avoid probabilities to be exactly zero or one
+    def __clip_probs(self):
+        self.__p = np.clip(self.__p, 1e-10, 1-(1e-10))
     
+
     # Find polynomial fit using forward selection process
     def __forward_select(self, alpha, maxDeg):
         family = sm.families.Binomial()
@@ -277,7 +282,7 @@ class CalibrationBelt():
 
             if n > m_start:
                 # Log-likelihood ratio test
-                Dm = 2 * (fit.llf - fit_new.llf)
+                Dm = (2 * (fit.llf - fit_new.llf))
 
                 # Use previous order for m if model does not improve
                 if Dm < inv_chi2:
@@ -289,7 +294,6 @@ class CalibrationBelt():
             n += 1
 
             fit_formula += " + "
-
         return(fit, m)
 
 